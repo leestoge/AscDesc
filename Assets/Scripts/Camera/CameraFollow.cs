a7057ef48320784b98c1cd0547317b37b9448094
Lee <L918Sturrock@hotmail.com>
2019-04-24 15:33:01 +0100

    Created camera follow script for Ascend gamemode

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform target;
+    public float smoothSpeed = 0.3f;
+
+    private Vector3 currentVelocity;
+
+    void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target.position.y > transform.position.y)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, newPos, ref currentVelocity, smoothSpeed *Time.deltaTime);
+        }
+    }
+}
