599043c641c78def2ac2dbeb37915c5fe00766f8
Lee <L918Sturrock@hotmail.com>
2019-04-24 05:32:19 +0100

    Added player jumping

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,14 +3,30 @@
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-
-    public float moveSpeed = 2f;
-
+    private bool isGrounded;
+    [HideInInspector]
     public bool playerMoving;
+
+    public Transform feetPos;
+    public float checkRadius;
+    public float moveSpeed = 2f;
+    public float jumpForce = 1f;   
+    public LayerMask whatIsGround;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
     }
 
     // Update is called once per frame
