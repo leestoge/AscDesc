8d31993865540ea7e7922028d70de10f49d7289d
Lee <L918Sturrock@hotmail.com>
2019-04-24 15:32:41 +0100

    Set up Ascend gamemode movement

---
+++ Assets/Scripts/Player/Ascend/PlayerMovementASC.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovementASC : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float movement;
+
+    public bool playerMoving;
+    public float movementSpeed = 10f;
+
+    void Awake()
+    {
+        playerMoving = false;
+        rb = GetComponent<Rigidbody2D>();
+        movement = 0f;
+    }
+
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal") * movementSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        if (movement < 0f || movement > 0f)
+        {
+            playerMoving = true;
+            Vector2 velocity = rb.velocity;
+            velocity.x = movement;
+            rb.velocity = velocity;
+        }
+        else
+        {
+            playerMoving = false;
+        }
+    }
+}
