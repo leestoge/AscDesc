85250c20347f65bb30f74ea9188d7b2f33f3beea
Lee <L918Sturrock@hotmail.com>
2019-04-24 15:34:59 +0100

    Finalized ascend gamemode platform script

--- Assets/Scripts/Platforms/Ascend/PlatformAsc.cs
+++ Assets/Scripts/Platforms/Ascend/PlatformAsc.cs
@@ -6,13 +6,16 @@ public class PlatformAsc : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
-
-        if (rb != null)
+        if (collision.relativeVelocity.y <= 0)
         {
-            Vector2 velocity = rb.velocity;
-            velocity.y = jumpForce;
-            rb.velocity = velocity;
+            Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                Vector2 velocity = rb.velocity;
+                velocity.y = jumpForce;
+                rb.velocity = velocity;
+            }
         }
     }
 }
